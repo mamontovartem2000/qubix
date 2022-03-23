71331a324217a89389261b3fe51137b7871c4f21
Dima <dskeygard@mail.ru>
2022-03-23 19:50:11 +0300

    Laser

---
+++ Assets/Dima/Scripts/TeleportParticle.cs
@@ -0,0 +1,24 @@
+﻿using ME.ECS;
+
+namespace Dima.Scripts
+{
+    using ME.ECS.Views.Providers;
+    
+    public class TeleportParticle : ParticleViewSource<ApplyTeleportParticleStateParticle> { }
+
+    [System.Serializable]
+    public class ApplyTeleportParticleStateParticle : ParticleView<ApplyTeleportParticleStateParticle>
+    {
+        public override void OnInitialize() { }
+        public override void OnDeInitialize() { }
+        public override void ApplyStateJob(float deltaTime, bool immediately)
+        {
+            ref var rootData = ref this.GetRootData();
+            rootData.position = entity.GetPosition();
+            rootData.startSize = 1f;
+            this.SetRootData(ref rootData);
+        }
+        public override void ApplyState(float deltaTime, bool immediately) { }
+    }
+
+}
