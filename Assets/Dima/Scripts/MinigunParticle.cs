15a26540c0e3cd616ace9a43a97f38d764a7b657
Dima <dskeygard@mail.ru>
2022-03-31 18:55:51 +0300

    newVFXpack

---
+++ Assets/Dima/Scripts/MinigunParticle.cs
@@ -0,0 +1,24 @@
+﻿using ME.ECS;
+
+namespace Assets.Dima.Scripts
+{
+    using ME.ECS.Views.Providers;
+
+    public class MinigunParticle : ParticleViewSource<ApplyMinigunParticleStateParticle> { }
+
+    [System.Serializable]
+    public class ApplyMinigunParticleStateParticle : ParticleView<ApplyMinigunParticleStateParticle>
+    {
+        public override void OnInitialize() { }
+        public override void OnDeInitialize() { }
+        public override void ApplyStateJob(float deltaTime, bool immediately)
+        {
+            ref var rootData = ref this.GetRootData();
+            rootData.position = entity.GetPosition();
+            rootData.startSize = 1f;
+            this.SetRootData(ref rootData);
+        }
+        public override void ApplyState(float deltaTime, bool immediately) { }
+    }
+}
+    
