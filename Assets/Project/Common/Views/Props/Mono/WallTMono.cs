d7ff29a9f1b4e10b3e593e63119b14fa2620ff61
Dima <dskeygard@mail.ru>
2022-04-19 15:48:29 +0300

    Grand visual update

---
+++ Assets/Project/Common/Views/Props/Mono/WallTMono.cs
@@ -0,0 +1,20 @@
+﻿using ME.ECS;
+
+namespace Assets.Project.Common.Views.Props.Mono
+{
+    using ME.ECS.Views.Providers;
+
+    public class WallTMono : MonoBehaviourView
+    {
+
+        public override bool applyStateJob => true;
+        public override void OnInitialize() { }
+        public override void OnDeInitialize() { }
+        public override void ApplyStateJob(UnityEngine.Jobs.TransformAccess transform, float deltaTime, bool immediately) { }
+        public override void ApplyState(float deltaTime, bool immediately)
+        {
+            transform.position = entity.GetPosition();
+            transform.rotation = entity.GetRotation();
+        }
+    }
+}
---
+++ Assets/Project/NewVisuals/Scripts/MapChanger.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class MapChanger : MonoBehaviour
+{
+    [SerializeField] public Maps Maps;
+    [SerializeField] private Material[] _mainFloors;
+    [SerializeField] private Material[] _extraObjects;
+    [SerializeField] private Material[] _props;
+    [SerializeField] private Material[] _walls;
+    [SerializeField] private Color[] _colorPalletes;
+    [SerializeField] private Color[] _emissionColorPalletes;
+
+    private void Start()
+    {
+        ChangeMap();
+    }
+
+    public void ChangeMap()
+    {
+        switch (Maps)
+        {
+            case Maps.Black:
+                ChangeColor(28, 15, 0);
+                ChangeEmissionColor(7, 7, 1);
+                ExtraChangeColors(1, 2, 3, 4, 5);
+                ExtraChangeEmissionColors(7,6,5,4,3);
+                break;
+            case Maps.Coliseum:
+                ChangeColor(7, 22, 7);
+                ChangeEmissionColor(0, 4, 0);
+                ExtraChangeColors(8, 7, 6, 5, 4);
+                ExtraChangeEmissionColors(0,1,2,3,4);
+                break;
+        }
+    }
+
+    private void ChangeColor(int floorColor, int propColor, int wallColor)
+    {
+        foreach (var item in _mainFloors) { item.SetColor("_BaseColor", _colorPalletes[floorColor]); }
+        foreach (var item in _props) { item.SetColor("_BaseColor", _colorPalletes[propColor]); }
+        foreach (var item in _walls) { item.SetColor("_BaseColor", _colorPalletes[wallColor]); }
+    }
+
+    private void ChangeEmissionColor(int floorColor, int propColor, int wallColor)
+    {
+        foreach (var item in _mainFloors) { item.SetColor("_EmissionColor", _emissionColorPalletes[floorColor] * 5); }
+        foreach (var item in _props) { item.SetColor("_EmissionColor", _emissionColorPalletes[propColor] * 5); }
+        foreach (var item in _walls) { item.SetColor("_EmissionColor", _emissionColorPalletes[wallColor] * 5); }
+    }
+
+    private void ExtraChangeColors(int a, int b, int c, int d, int e)
+    {
+        _extraObjects[0].SetColor("_BaseColor", _colorPalletes[a - 1]); //bridge
+        _extraObjects[1].SetColor("_BaseColor", _colorPalletes[a]);
+        _extraObjects[2].SetColor("_BaseColor", _colorPalletes[b]);
+        _extraObjects[3].SetColor("_BaseColor", _colorPalletes[c]);
+        _extraObjects[4].SetColor("_BaseColor", _colorPalletes[d]);
+        _extraObjects[5].SetColor("_BaseColor", _colorPalletes[e]);
+    }
+    private void ExtraChangeEmissionColors(int a, int b, int c, int d, int e)
+    {
+        _extraObjects[1].SetColor("_EmissionColor", _emissionColorPalletes[a] * 15);
+        _extraObjects[2].SetColor("_EmissionColor", _emissionColorPalletes[b] * 15);
+        _extraObjects[3].SetColor("_EmissionColor", _emissionColorPalletes[c] * 15);
+        _extraObjects[4].SetColor("_EmissionColor", _emissionColorPalletes[d] * 15);
+        _extraObjects[5].SetColor("_EmissionColor", _emissionColorPalletes[e] * 15);
+    }
+}
+
+[System.Serializable]
+public enum Maps
+{
+    Black = 0,
+    Coliseum = 1
+}
