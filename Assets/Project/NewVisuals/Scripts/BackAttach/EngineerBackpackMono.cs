7f9f67f166e15525c31441cada2d91ab805e790b
Dima <dskeygard@mail.ru>
2022-04-26 12:57:00 +0300

    Views

---
+++ Assets/Project/NewVisuals/Scripts/BackAttach/EngineerBackpackMono.cs
@@ -0,0 +1,19 @@
+﻿using ME.ECS;
+
+namespace Project.NewVisuals.Scripts.BackAttach
+{
+    using ME.ECS.Views.Providers;
+
+    public class EngineerBackpackMono : MonoBehaviourView
+    {
+        public override bool applyStateJob => true;
+        public override void OnInitialize() { }
+        public override void OnDeInitialize() { }
+        public override void ApplyStateJob(UnityEngine.Jobs.TransformAccess transform, float deltaTime, bool immediately) { }
+        public override void ApplyState(float deltaTime, bool immediately)
+        {
+            transform.position = entity.GetPosition();
+            transform.rotation = entity.GetRotation();
+        }
+    }
+}
