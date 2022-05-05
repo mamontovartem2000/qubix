0233de5d369d8610314f4cf95c21975cfea0c2a2
Dima <dskeygard@mail.ru>
2022-05-05 16:58:15 +0300

    FixLight

--- Assets/Project/Visuals/Scripts/MapChangerInput.cs
+++ Assets/Project/Visuals/Scripts/MapChangerInput.cs
@@ -1,21 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-public class MapChangerInput : MonoBehaviour
-{
-    [SerializeField] private MapChanger _mapChanger;
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            _mapChanger.Maps = Maps.Black;
-            _mapChanger.ChangeMap();
-        }
-        if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            _mapChanger.Maps = Maps.Coliseum;
-            _mapChanger.ChangeMap();
-        }
-    }
-}
+// public class MapChangerInput : MonoBehaviour
+// {
+//     [SerializeField] private MapChanger _mapChanger;
+//     private void Update()
+//     {
+//         if (Input.GetKeyDown(KeyCode.LeftArrow))
+//         {
+//             _mapChanger.Maps = Maps.Black;
+//             _mapChanger.ChangeMap();
+//         }
+//         if (Input.GetKeyDown(KeyCode.RightArrow))
+//         {
+//             _mapChanger.Maps = Maps.Coliseum;
+//             _mapChanger.ChangeMap();
+//         }
+//     }
+// }
