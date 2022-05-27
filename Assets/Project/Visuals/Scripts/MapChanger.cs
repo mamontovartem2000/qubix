953b4a833c122869c35d94fdb0878f3fa04dc028
Dima <dskeygard@mail.ru>
2022-05-27 21:47:56 +0300

    NewTile

--- Assets/Project/Visuals/Scripts/MapChanger.cs
+++ Assets/Project/Visuals/Scripts/MapChanger.cs
@@ -8,6 +8,8 @@ public class MapChanger : MonoBehaviour
     [SerializeField] private Material[] _extraObjects;
     [SerializeField] private Material[] _props;
     [SerializeField] private Material[] _walls;
+    [SerializeField] private Material _bridge;
+    [SerializeField] private Material _triangleTile;
     [SerializeField] private Color[] _colorPalletes;
     [SerializeField] private Color[] _emissionColorPalletes;
     private float _emissionIntensity;
@@ -15,40 +17,30 @@ public class MapChanger : MonoBehaviour
     private void Awake()
     {
         Changer = this;
-        // ChangeMap(Maps.Coliseum);
+        ChangeMap(Maps.Coliseum);
     }
 
     public void ChangeMap(Maps map)
     {
         switch (map)
         {
-            case Maps.Black:
-                _emissionIntensity = 5;
-                ChangeColor(29, 15, 0);
-                ChangeEmissionColor(7, 7, 1);
-                ExtraChangeColors(1, 2, 3, 4, 5);
-                ExtraChangeEmissionColors(7, 7, 6, 5, 4, 3);
-                break;
             case Maps.Coliseum:
                 _emissionIntensity = 3;
                 ChangeColor(20, 15, 0);
                 ChangeEmissionColor(4, 4, 4);
                 ExtraChangeColors(19, 19, 30, 4, 5);
-                ExtraChangeEmissionColors(4, 4, 6, 5, 4, 4);
-                break;
-            case Maps.Forest:
-                _emissionIntensity = 5;
-                ChangeColor(13, 22, 14);
-                ChangeEmissionColor(3, 2, 3);
-                ExtraChangeColors(13, 13, 13, 13, 13);
-                ExtraChangeEmissionColors(3, 3, 3, 3, 3, 3);
+                ExtraChangeEmissionColors(4, 6, 5, 4, 4);
+                ChangeBridge(1, 1);
+                ChangeTriangleTile(1, 1);
                 break;
             case Maps.Neon:
                 _emissionIntensity = 6;
                 ChangeColor(23, 19, 19);
                 ChangeEmissionColor(5, 5, 5);
                 ExtraChangeColors(20, 20, 4, 19, 30);
-                ExtraChangeEmissionColors(5, 5, 5, 5, 5, 5);
+                ExtraChangeEmissionColors(5, 5, 5, 5, 5);
+                ChangeBridge(1, 1);
+                ChangeTriangleTile(1, 1);
                 break;
         }
     }
@@ -69,29 +61,37 @@ public class MapChanger : MonoBehaviour
 
     private void ExtraChangeColors(int a, int b, int c, int d, int e)
     {
-        _extraObjects[0].SetColor("_BaseColor", _colorPalletes[a - 1]); //bridge
-        _extraObjects[1].SetColor("_BaseColor", _colorPalletes[a]);
-        _extraObjects[2].SetColor("_BaseColor", _colorPalletes[b]);
-        _extraObjects[3].SetColor("_BaseColor", _colorPalletes[c]);
-        _extraObjects[4].SetColor("_BaseColor", _colorPalletes[d]);
-        _extraObjects[5].SetColor("_BaseColor", _colorPalletes[e]);
+        _extraObjects[0].SetColor("_BaseColor", _colorPalletes[a]);
+        _extraObjects[1].SetColor("_BaseColor", _colorPalletes[b]);
+        _extraObjects[2].SetColor("_BaseColor", _colorPalletes[c]);
+        _extraObjects[3].SetColor("_BaseColor", _colorPalletes[d]);
+        _extraObjects[4].SetColor("_BaseColor", _colorPalletes[e]);
     }
-    private void ExtraChangeEmissionColors(int o, int a, int b, int c, int d, int e)
+    private void ExtraChangeEmissionColors(int a, int b, int c, int d, int e)
     {
-        _extraObjects[0].SetColor("_EmissionColor", _emissionColorPalletes[o] * _emissionIntensity);
-        _extraObjects[1].SetColor("_EmissionColor", _emissionColorPalletes[a] * _emissionIntensity);
-        _extraObjects[2].SetColor("_EmissionColor", _emissionColorPalletes[b] * _emissionIntensity);
-        _extraObjects[3].SetColor("_EmissionColor", _emissionColorPalletes[c] * _emissionIntensity);
-        _extraObjects[4].SetColor("_EmissionColor", _emissionColorPalletes[d] * _emissionIntensity);
-        _extraObjects[5].SetColor("_EmissionColor", _emissionColorPalletes[e] * _emissionIntensity);
+        _extraObjects[0].SetColor("_EmissionColor", _emissionColorPalletes[a] * _emissionIntensity);
+        _extraObjects[1].SetColor("_EmissionColor", _emissionColorPalletes[b] * _emissionIntensity);
+        _extraObjects[2].SetColor("_EmissionColor", _emissionColorPalletes[c] * _emissionIntensity);
+        _extraObjects[3].SetColor("_EmissionColor", _emissionColorPalletes[d] * _emissionIntensity);
+        _extraObjects[4].SetColor("_EmissionColor", _emissionColorPalletes[e] * _emissionIntensity);
+    }
+
+    private void ChangeBridge(int baseColor, int emission)
+    {
+        _bridge.SetColor("_BaseColor", _colorPalletes[baseColor]);
+        _bridge.SetColor("_EmissionColor", _emissionColorPalletes[emission] * _emissionIntensity);
+    }
+
+    private void ChangeTriangleTile(int baseColor, int emission)
+    {
+        _triangleTile.SetColor("_BaseColor", _colorPalletes[baseColor]);
+        _triangleTile.SetColor("_EmissionColor", _emissionColorPalletes[emission] * _emissionIntensity);
     }
 }
 
 [System.Serializable]
 public enum Maps
 {
-    Black = 0,
-    Coliseum = 1,
-    Forest = 2,
-    Neon = 3,
+    Coliseum,
+    Neon,
 }
