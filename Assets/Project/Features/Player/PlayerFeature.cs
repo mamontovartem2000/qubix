510d7315cf2a1ef9b789b5c61b427fb867c92381
Grigoriy Elovikov <elovikovgri@yandex.ru>
2022-07-29 14:49:44 +0300

    Rocket launcher add

--- Assets/Project/Features/Player/PlayerFeature.cs
+++ Assets/Project/Features/Player/PlayerFeature.cs
@@ -16,124 +16,139 @@ namespace Project.Features.Player
      Unity.IL2CPP.CompilerServices.Il2CppSetOptionAttribute(Unity.IL2CPP.CompilerServices.Option.DivideByZeroChecks, false)]
 #endif
     public sealed class PlayerFeature : Feature
-	{
-		public PlayerInput PlayerInput;
-		public DataConfig BullerConfig;
-		public DataConfig GoldHunterConfig;
-		public DataConfig PowerfConfig;
-		public DataConfig SilenConfig;
-		public DataConfig SolarayConfig;
-		public DataConfig BloodlovConfig;
+    {
+        public PlayerInput PlayerInput;
+        public DataConfig BullerConfig;
+        public DataConfig GoldHunterConfig;
+        public DataConfig PowerfConfig;
+        public DataConfig SilenConfig;
+        public DataConfig SolarayConfig;
+        public DataConfig BloodlovConfig;
+        public DataConfig LomixConfig;
 
-		private RPCId _onPlayerConnected, _onPlayerDisconnected;
-		private Filter _playerFilter;
+        private RPCId _onPlayerConnected, _onPlayerDisconnected;
+        private Filter _playerFilter;
 
-		protected override void OnConstruct()
-		{
-			AddModule<PlayerConnectionModule>();
-			AddSystem<HandleInputSystem>();
-			
-			PlayerInput = new PlayerInput();
-			PlayerInput.Enable();
-			
-			Filter.Create("Player-Filter")
-				.With<PlayerTag>()
-				.Push(ref _playerFilter);
+        protected override void OnConstruct()
+        {
+            AddModule<PlayerConnectionModule>();
+            AddSystem<HandleInputSystem>();
 
-			var net = world.GetModule<NetworkModule>();
-			net.RegisterObject(this);
+            PlayerInput = new PlayerInput();
+            PlayerInput.Enable();
 
-			_onPlayerConnected = net.RegisterRPC(new System.Action<NetworkSetActivePlayer>(PlayerConnected_RPC).Method);
-			_onPlayerDisconnected = net.RegisterRPC(new System.Action<int>(PlayerDisconnected_RPC).Method);
-		}
+            Filter.Create("Player-Filter")
+                .With<PlayerTag>()
+                .Push(ref _playerFilter);
 
-		public void OnLocalPlayerConnected(NetworkSetActivePlayer marker)
-		{
-			var net = world.GetModule<NetworkModule>();
-			net.RPC(this, _onPlayerConnected, marker);
-		}
+            var net = world.GetModule<NetworkModule>();
+            net.RegisterObject(this);
 
-		private void PlayerConnected_RPC(NetworkSetActivePlayer nsap)
-		{
-			var localId = nsap.ActorLocalID;
-			var player = new Entity("player_" + localId);
-			player.Set(new PlayerTag {PlayerLocalID = localId, PlayerServerID = nsap.ServerID, Nickname = nsap.Nickname });
-			player.Set(new TeamTag { Value = nsap.Team });
+            _onPlayerConnected = net.RegisterRPC(new System.Action<NetworkSetActivePlayer>(PlayerConnected_RPC).Method);
+            _onPlayerDisconnected = net.RegisterRPC(new System.Action<int>(PlayerDisconnected_RPC).Method);
+        }
 
-			world.GetFeature<EventsFeature>().TabulationAddPlayer.Execute(player);
+        public void OnLocalPlayerConnected(NetworkSetActivePlayer marker)
+        {
+            var net = world.GetModule<NetworkModule>();
+            net.RPC(this, _onPlayerConnected, marker);
+        }
+
+        private void PlayerConnected_RPC(NetworkSetActivePlayer nsap)
+        {
+            var localId = nsap.ActorLocalID;
+            var player = new Entity("player_" + localId);
+            player.Set(new PlayerTag
+                {PlayerLocalID = localId, PlayerServerID = nsap.ServerID, Nickname = nsap.Nickname});
+            player.Set(new TeamTag {Value = nsap.Team});
+
+            world.GetFeature<EventsFeature>().TabulationAddPlayer.Execute(player);
 
 
-			if (NetworkData.PlayersInfo == null) // Fake case
-			{
-				BloodlovConfig.Apply(player);
-				return;
-			}
+            if (NetworkData.PlayersInfo == null) // Fake case
+            {
+                LomixConfig.Apply(player);
+                return;
+            }
 
-			switch (NetworkData.PlayersInfo[localId].Character)
-			{
-				case "Buller":
-					{
-						BullerConfig.Apply(player);
-						break;
-					}
-				case "GoldHunter":
-					{
-						GoldHunterConfig.Apply(player);
-						break;
-					}
-				case "Powerf":
-					{
-						PowerfConfig.Apply(player);
-						break;
-					}
-				case "Silen":
-					{
-						SilenConfig.Apply(player);
-						break;
-					}
-				case "Solaray":
-					{
-						SolarayConfig.Apply(player);
-						break;
-					}
-				case "Bloodlov":
-					{
-						BloodlovConfig.Apply(player);
-						break;
-					}
-			}
-		}
+            switch (NetworkData.PlayersInfo[localId].Character)
+            {
+                case "Buller":
+                {
+                    BullerConfig.Apply(player);
+                    break;
+                }
+                
+                case "GoldHunter":
+                {
+                    GoldHunterConfig.Apply(player);
+                    break;
+                }
+                
+                case "Powerf":
+                {
+                    PowerfConfig.Apply(player);
+                    break;
+                }
+                
+                case "Silen":
+                {
+                    SilenConfig.Apply(player);
+                    break;
+                }
+                
+                case "Solaray":
+                {
+                    SolarayConfig.Apply(player);
+                    break;
+                }
+                
+                case "Bloodlov":
+                {
+                    BloodlovConfig.Apply(player);
+                    break;
+                }
+                
+                case "Lomix":
+                {
+                    LomixConfig.Apply(player);
+                    break;
+                }
+            }
+        }
 
-		public void OnLocalPlayerDisconnected(int id)
-		{
-			var net = world.GetModule<NetworkModule>();
-			net.RPC(this, _onPlayerDisconnected, id);
-		}
+        public void OnLocalPlayerDisconnected(int id)
+        {
+            var net = world.GetModule<NetworkModule>();
+            net.RPC(this, _onPlayerDisconnected, id);
+        }
 
-		private void PlayerDisconnected_RPC(int id)
-		{
-			Debug.Log("Destroy Avatar");
-			ref var avatar = ref GetPlayerByID(id).Get<PlayerAvatar>().Value;
-			ref var weapons = ref avatar.Get<WeaponEntities>();
-			
-			weapons.LeftWeapon.Destroy();
-			weapons.RightWeapon.Destroy();
-			avatar.Destroy();
-		}
+        private void PlayerDisconnected_RPC(int id)
+        {
+            Debug.Log("Destroy Avatar");
+            ref var avatar = ref GetPlayerByID(id).Get<PlayerAvatar>().Value;
+            ref var weapons = ref avatar.Get<WeaponEntities>();
 
-		public Entity GetPlayerByID(int id)
-		{
-			foreach (var player in _playerFilter)
-			{
-				if (player.Read<PlayerTag>().PlayerLocalID == id)
-				{
-					return player;
-				}
-			}
+            weapons.LeftWeapon.Destroy();
+            weapons.RightWeapon.Destroy();
+            avatar.Destroy();
+        }
 
-			return Entity.Empty;
-		}
+        public Entity GetPlayerByID(int id)
+        {
+            foreach (var player in _playerFilter)
+            {
+                if (player.Read<PlayerTag>().PlayerLocalID == id)
+                {
+                    return player;
+                }
+            }
 
-		protected override void OnDeconstruct() {}
-	}
+            return Entity.Empty;
+        }
+
+        protected override void OnDeconstruct()
+        {
+        }
+    }
 }
